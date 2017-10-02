75530d01e5d1631aa90c12df75f79260dbf0f0e4
Marc Johnson <marc@k-int.co.uk>
2017-10-02 10:15:34 +0100

    Introduce Request API constructor for initialising collaborators

--- src/main/java/org/folio/rest/impl/support/SimpleLoggingAssistant.java
+++ src/main/java/org/folio/rest/impl/support/SimpleLoggingAssistant.java
@@ -2,7 +2,8 @@ package org.folio.rest.impl.support;
 
 import io.vertx.core.logging.Logger;
 
-public class SimpleLoggingAssistant {
+public class SimpleLoggingAssistant implements LoggingAssistant {
+  @Override
   public void logError(Logger logger, Throwable error) {
     logger.error(error.getMessage(), error);
   }
