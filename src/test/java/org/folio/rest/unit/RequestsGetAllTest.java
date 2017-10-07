e625d0d543e4088f84e15b30276a70f507d2943c
Marc Johnson <marc@k-int.co.uk>
2017-10-07 16:57:00 +0100

    Split get and delete all requests unit tests

--- src/test/java/org/folio/rest/unit/RequestsGetAllTest.java
+++ src/test/java/org/folio/rest/unit/RequestsGetAllTest.java
@@ -1,15 +1,12 @@
 package org.folio.rest.unit;
 
 import io.vertx.core.AsyncResult;
-import io.vertx.core.Context;
-import io.vertx.core.Vertx;
 import org.folio.rest.impl.RequestsAPI;
 import org.folio.rest.impl.support.LoggingAssistant;
 import org.folio.rest.impl.support.storage.Storage;
 import org.folio.rest.jaxrs.model.Request;
+import org.folio.rest.unit.support.AbstractVertxUnitTest;
 import org.folio.rest.unit.support.SampleParameters;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response;
@@ -24,125 +21,7 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-public class RequestsAPITest {
-
-  private static final String TENANT_ID = "test_tenant";
-
-  private static Vertx vertx;
-  private static Context context;
-
-  @BeforeClass
-  public static void before() {
-    vertx = Vertx.vertx();
-    context = vertx.getOrCreateContext();
-  }
-
-  @AfterClass
-  public static void after() {
-    vertx.close();
-  }
-
-  @Test
-  public void shouldLogFailureDuringDeleteAll() throws Exception {
-    LoggingAssistant mockLogAssistant = mock(LoggingAssistant.class);
-    Storage mockStorage = mock(Storage.class);
-
-    RequestsAPI requestsAPI = new RequestsAPI(mockStorage, mockLogAssistant);
-
-    Exception expectedException = new Exception("Sample Failure");
-
-    fail(expectedException, 2).when(mockStorage).deleteAll(eq(context), eq(TENANT_ID), any());
-
-    AsyncResult<Response> response = getOnCompletion(f ->
-      requestsAPI.deleteRequestStorageRequests(
-        SampleParameters.sampleLanguage(),
-        SampleParameters.sampleHeaders(TENANT_ID),
-        complete(f),
-        context));
-
-    assertThat(response.result().getStatus(), is(500));
-
-    verify(mockLogAssistant, times(1)).logError(any(), eq(expectedException));
-    verify(mockLogAssistant, never()).logError(any(), any(String.class));
-
-    verify(mockStorage, times(1)).deleteAll(eq(context), eq(TENANT_ID), any());
-  }
-
-  @Test
-  public void shouldLogUnknownFailureDuringDeleteAll() throws Exception {
-    LoggingAssistant mockLogAssistant = mock(LoggingAssistant.class);
-    Storage mockStorage = mock(Storage.class);
-
-    RequestsAPI requestsAPI = new RequestsAPI(mockStorage, mockLogAssistant);
-
-    fail(null, 2).when(mockStorage).deleteAll(eq(context), eq(TENANT_ID), any());
-
-    AsyncResult<Response> response = getOnCompletion(f ->
-      requestsAPI.deleteRequestStorageRequests(
-        SampleParameters.sampleLanguage(),
-        SampleParameters.sampleHeaders(TENANT_ID),
-        complete(f),
-        context));
-
-    assertThat(response.result().getStatus(), is(500));
-
-    verify(mockLogAssistant, times(1)).logError(any(),
-      eq("Unknown failure cause when attempting to delete all requests"));
-
-    verify(mockLogAssistant, never()).logError(any(), any(Throwable.class));
-
-    verify(mockStorage, times(1)).deleteAll(eq(context), eq(TENANT_ID), any());
-  }
-
-  @Test
-  public void shouldLogUnexpectedExceptionDuringDeleteAll() throws Exception {
-    LoggingAssistant mockLogAssistant = mock(LoggingAssistant.class);
-    Storage mockStorage = mock(Storage.class);
-
-    RequestsAPI requestsAPI = new RequestsAPI(mockStorage, mockLogAssistant);
-
-    Exception expectedException = new Exception("Sample Failure");
-
-    doThrow(expectedException).when(mockStorage).deleteAll(eq(context), eq(TENANT_ID), any());
-
-    AsyncResult<Response> response = getOnCompletion(f ->
-      requestsAPI.deleteRequestStorageRequests(
-        SampleParameters.sampleLanguage(),
-        SampleParameters.sampleHeaders(TENANT_ID),
-        complete(f),
-        context));
-
-    assertThat(response.result().getStatus(), is(500));
-
-    verify(mockLogAssistant, times(1)).logError(any(), eq(expectedException));
-    verify(mockLogAssistant, never()).logError(any(), any(String.class));
-
-    verify(mockStorage, times(1)).deleteAll(eq(context), eq(TENANT_ID), any());
-  }
-
-  @Test
-  public void shouldNotLogDuringSuccessfulDeleteAll() throws Exception {
-    LoggingAssistant mockLogAssistant = mock(LoggingAssistant.class);
-    Storage mockStorage = mock(Storage.class);
-
-    RequestsAPI requestsAPI = new RequestsAPI(mockStorage, mockLogAssistant);
-
-    succeed("", 2).when(mockStorage).deleteAll(eq(context), eq(TENANT_ID), any());
-
-    AsyncResult<Response> response = getOnCompletion(f ->
-      requestsAPI.deleteRequestStorageRequests(
-        SampleParameters.sampleLanguage(),
-        SampleParameters.sampleHeaders(TENANT_ID),
-        complete(f),
-        context));
-
-    assertThat(response.result().getStatus(), is(204));
-
-    verify(mockLogAssistant, never()).logError(any(), any(String.class));
-    verify(mockLogAssistant, never()).logError(any(), any(Throwable.class));
-
-    verify(mockStorage, times(1)).deleteAll(eq(context), eq(TENANT_ID), any());
-  }
+public class RequestsGetAllTest extends AbstractVertxUnitTest {
 
   @Test
   public void shouldLogFailureDuringGetAll() throws Exception {
